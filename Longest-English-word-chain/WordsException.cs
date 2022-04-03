1fd19989645d00b529ea5e8f31a07eefd131c61f
战晨曦 <18373466@buaa.edu.cn>
2022-04-03 13:11:07 +0800

    分离Core和其他部分的异常

--- Longest-English-word-chain/WordsException.cs
+++ Longest-English-word-chain/WordsException.cs
@@ -14,18 +14,4 @@ namespace Core
 		{
 		}
 	}
-
-	public class ResultTooLongException : Exception
-	{
-		public ResultTooLongException(string message) : base("The result is too long!" + message)
-		{
-		}
-	}
-
-	public class HasImplicitLoopException : Exception
-	{
-		public HasImplicitLoopException(string message) : base("The input words have Implicit loop!")
-		{
-		}
-	}
 }
