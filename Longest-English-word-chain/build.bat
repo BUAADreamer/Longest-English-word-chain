3d3e9b64b8f6b6f7a84210252cb7aa044ca4529b
BUAADreamer <1428195643@qq.com>
2022-04-03 13:53:38 +0800

    modify build.bat

--- Longest-English-word-chain/build.bat
+++ Longest-English-word-chain/build.bat
@@ -1,5 +1,5 @@
 csc /target:library /out:Library.dll .\Input.cs .\Output.cs .\LibraryException.cs
 csc /target:library /out:Core.dll .\Core.cs .\CoreException.cs .\PairTestInterface.cs
 csc /out:WordList.exe *.cs
-cp .\Core.dll ..\guiproject\guiproject\
-cp .\Library.dll ..\guiproject\guiproject\
+xcopy .\Core.dll ..\guiproject\guiproject\
+xcopy .\Library.dll ..\guiproject\guiproject\
